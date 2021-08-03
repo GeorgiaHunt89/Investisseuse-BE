dbd8205053e5e5b89a17586a553e5d3e9d8b427c
Georgia Hunt <georgialhunt89@gmail.com>
2021-08-03 21:03:58 +0930

    Initial commit

---
+++ models/index.js
@@ -0,0 +1,9 @@
+const User = require("./User");
+const BusinessListing = require("./BusinessListing");
+const InvestmentOptions = require("./InvestmentOptions");
+
+module.exports = {
+  User,
+  BusinessListing,
+  InvestmentOptions,
+};
